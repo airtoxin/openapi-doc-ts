510c88aff150f39ae4787f855f4a37171cee7b6a
airtoxin <airtoxin@icloud.com>
2020-09-07 23:57:24 +0900

    Add sample

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 import { spec } from "./spec";
-import "swagger-ui-react/swagger-ui.css"
+import "swagger-ui-react/swagger-ui.css";
 
-async function startApplication(){
+async function startApplication() {
   await import("refractor");
   const SwaggerUI = (await import("swagger-ui-react")).default;
-  ReactDOM.render(<SwaggerUI spec={spec} />, document.getElementById('app'));
+  ReactDOM.render(<SwaggerUI spec={spec} />, document.getElementById("app"));
 }
 
 startApplication();
--- src/spec.ts
+++ src/spec.ts
@@ -2,4 +2,40 @@ import { OpenApiBuilder } from "openapi3-ts";
 
 const builder = new OpenApiBuilder();
 
+builder.addTitle("Example API document");
+builder.addDescription("Build OpenAPI3 api document with type support.");
+builder.addVersion("1.0.0");
+
+builder.addServer({ url: "https://example.com", description: "production" });
+builder.addServer({
+  url: "https://{env}.example.com",
+  description: "non production",
+  variables: { env: { enum: ["dev", "stg"], default: "dev" } },
+});
+
+builder.addPath("/example", {
+  description: "example resource",
+  get: {
+    responses: {
+      200: {
+        description: "success",
+        content: {
+          "application/json": {
+            schema: {
+              title: "Example object",
+              type: "object",
+              properties: {
+                a: {
+                  nullable: false,
+                  type: "number",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
 export const spec = builder.getSpec();
