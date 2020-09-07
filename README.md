c93c240a3fb5540034771624da53060dd480b6ec
airtoxin <airtoxin@icloud.com>
2020-09-08 00:02:00 +0900

    Add doc

---
+++ README.md
@@ -0,0 +1,3 @@
+# openapi-doc-ts
+
+Example repository of writing OpenAPI3 api document with type supports.
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { spec } from "./spec";
 import "swagger-ui-react/swagger-ui.css";
 
 async function startApplication() {
+  // Workaround for Cannot find module '../node_modules/refractor/lang/markup.js'
+  // https://github.com/parcel-bundler/parcel/issues/3176#issuecomment-642568855
   await import("refractor");
   const SwaggerUI = (await import("swagger-ui-react")).default;
   ReactDOM.render(<SwaggerUI spec={spec} />, document.getElementById("app"));
